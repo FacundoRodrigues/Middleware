fed60d717a95c5485efdbb2590cbe69b1be6a0c6
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-08-23 09:51:35 -0300

    23-8-23: add project api and blank solution.
    add first approach for implement Middleware

---
+++ Middleware/API/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configure Logging providers
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//Option 1: Adding Middleware With Request Delegates
+app.Use(async (httpContext, next) =>
+{
+    try
+    {
+        await next(httpContext);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, ex.Message);
+    }
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
